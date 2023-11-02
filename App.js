e398eaf1d2be1947756219b76ef48b590914ba09
Muhammed-Adnaan <muhammedadnaan33@gmail.com>
2023-11-02 18:57:22 +0530

    use state and input txt

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
 export default function App() {
+  const[name, setname] = useState('adnaan')
+  const clickhander = () =>{
+    setname('afnaan');
+    setperson({name:'tamim' , age:'30'});
+  }
+  const[person, setperson] = useState({name:'akram', age:'40'});
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style = {styles.header}>
+      <Text style = {styles.boldtext}>Hello {name}!!</Text>
+        <Text>his name is {person.name} and his age is {person.age}</Text>
+      </View>
+      <View style ={styles.body}>
+        <Text style = {styles.boldtext}>Enter the age:</Text>
+        <TextInput style = {styles.input}
+        placeholder="Enter text here" />
+      </View>
+      <View style= {styles.btncontainer}>
+        <Button title='submit' onPress={clickhander}/>
+      </View>
     </View>
   );
 }
@@ -17,4 +34,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btncontainer:{
+
+  },
+  header:{
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldtext:{
+    fontWeight: 'bold',
+  },
+  body:{
+    backgroundColor: 'red',
+    padding: 20,
+  },
+  input:{
+    borderWidth: 3,
+    borderColor:'gray',
+    padding:20,
+    margin:10,
+
+  }
 });
